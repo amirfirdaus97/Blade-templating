a133f5add520492249cfcb4b5e6d97163c59ca29
amirfirdaus97 <74361901+amirfirdaus97@users.noreply.github.com>
2020-12-08 23:59:26 +0800

    Create about.blade.php

---
+++ about.blade.php
@@ -0,0 +1,9 @@
+@extends('/subview/master')
+@section('books', 'about')
+@section('content') 
+
+<h1>E-Books</h1>
+<br>
+    <h1>Basic Information about e-books:  {{ $Title }} 
+    </h1>
+@endsection 
